ec908369edd8772a3fa43c2deebd612bb2c2ef6f
pouma <pouma@DESKTOP-D9FAMPF>
2016-08-24 16:14:53 +0800

    fixed ARM, x64 not be support process arcithture

--- Library/MixpanelClientDotNet.UWP/Properties/AssemblyInfo.cs
+++ Library/MixpanelClientDotNet.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pou")]
 [assembly: AssemblyProduct("MixpanelDotNet.UWP")]
-[assembly: AssemblyCopyright("Copyright © Pou  2016")]
+[assembly: AssemblyCopyright("Copyright © Pou 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
 [assembly: ComVisible(false)]
