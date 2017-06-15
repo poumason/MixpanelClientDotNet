fafcbec71fb6a2670f60d558032605b27f7b1daf
pouma <pouma@DESKTOP-D9FAMPF>
2017-06-15 13:44:21 +0800

    add try ... catch to handle https service exception

--- Library/MixpanelClientDotNet.Shared/ServiceModel/Services/HttpService.cs
+++ Library/MixpanelClientDotNet.Shared/ServiceModel/Services/HttpService.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MixpanelDotNet.ServiceModel
@@ -18,11 +15,20 @@ namespace MixpanelDotNet.ServiceModel
 
         public async Task<string> SendRequest(string uri, string query)
         {
-            string url = $"{uri}?{query}";
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
-            var response = await httpClient.SendAsync(request);
-            string result = await response.Content.ReadAsStringAsync();
-            return result;
+            try
+            {
+                string url = $"{uri}?{query}";
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = await httpClient.SendAsync(request);
+                string result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                DebugLogger.Write(ex);
+
+                return string.Empty;
+            }
         }
     }
 }
